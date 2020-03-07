cfa8b97d4acf5fdc7800107982ba539b09f448ba
Troop 66 Webmaster <16478845+troop66matawan@users.noreply.github.com>
2020-03-07 13:43:11 -0500

    initial commit

---
+++ public/app.js
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * FirebaseUI initialization to be used in a Single Page application context.
+ */
+
+/**
+ * @return {!Object} The FirebaseUI config.
+ */
+function getUiConfig() {
+  return {
+    'callbacks': {
+      // Called when the user has been successfully signed in.
+      'signInSuccess': function(user, credential, redirectUrl) {
+        handleSignedInUser(user);
+        // Do not redirect.
+        return false;
+      }
+    },
+    // Opens IDP Providers sign-in flow in a popup.
+    'signInFlow': 'popup',
+    'signInOptions': [
+      // TODO(developer): Remove the providers you don't need for your app.
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // Whether the display name should be displayed in Sign Up page.
+        requireDisplayName: true
+      },
+    ],
+    // Terms of service url.
+    'tosUrl': 'https://www.google.com'
+  };
+}
+
+// Initialize the FirebaseUI Widget using Firebase.
+var firebaseauth = firebase.auth();
+var ui = new firebaseui.auth.AuthUI(window.firebaseauth);
+
+
+/**
+ * @return {string} The URL of the FirebaseUI standalone widget.
+ */
+function getWidgetUrl() {
+  return '/widget#recaptcha=' + getRecaptchaMode();
+}
+
+
+/**
+ * Redirects to the FirebaseUI widget.
+ */
+var signInWithRedirect = function() {
+  window.location.assign(getWidgetUrl());
+};
+
+
+/**
+ * Open a popup with the FirebaseUI widget.
+ */
+var signInWithPopup = function() {
+  window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
+};
+
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'block';
+  document.getElementById('user-signed-out').style.display = 'none';
+};
+
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  ui.start('#firebaseui-container', getUiConfig());
+  scouts = [];
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+
+/**
+ * Handles when the user changes the reCAPTCHA config.
+ */
+function handleRecaptchaConfigChange() {
+  var newRecaptchaValue = document.querySelector(
+      'input[name="recaptcha"]:checked').value;
+  location.replace(location.pathname + '#recaptcha=' + newRecaptchaValue);
+
+  // Reset the inline widget so the config changes are reflected.
+  ui.reset();
+  ui.start('#firebaseui-container', getUiConfig());
+}
+
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+
+};
+
+window.addEventListener('load', initApp);
